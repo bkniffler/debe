1ece99ce2af2b9824c339e7a6c441aee909c894c
Benjamin Kniffler <bkniffler@me.com>
2019-04-12 12:53:18 +0100

    add react binding

---
+++ src/react/index.test.tsx
@@ -0,0 +1,174 @@
+import * as React from 'react';
+import { act } from 'react-dom/test-utils';
+import { useAll, DebeProvider, useCollection } from './index';
+import { render, waitForElement, fireEvent } from 'react-testing-library';
+import { MemoryAdapter } from 'debe-memory';
+import 'jest-dom/extend-expect';
+
+interface ILorem {
+  name: string;
+}
+const collections = [{ name: 'lorem', index: ['name'] }];
+test('react:basic', async cb => {
+  function Component() {
+    const [result] = useAll<ILorem>('lorem', {});
+    return (
+      <span data-testid="result">{result.map(x => x.name).join(', ')}</span>
+    );
+  }
+
+  const { getByTestId, asFragment } = render(
+    <DebeProvider
+      initialize={async db => {
+        await db.insert('lorem', [{ name: 'Beni' }, { name: 'Alex' }]);
+      }}
+      collections={collections}
+      getAdapter={() => new MemoryAdapter()}
+      render={() => <Component />}
+      loading={() => <span>Loading...</span>}
+    />
+  );
+
+  await (act as any)(async () => {
+    await waitForElement(() => getByTestId('result'));
+  });
+
+  //expect(resultNode).toHaveTextContent();
+  expect(asFragment()).toMatchSnapshot();
+  cb();
+});
+
+test('react:many', async cb => {
+  function Component() {
+    const [result] = useAll<ILorem>('lorem', {
+      where: ['name < ?', '500']
+    });
+    return (
+      <ul data-testid="result">
+        <li>Start</li>
+        {result.map(item => (
+          <li key={item.id}>
+            <span data-testid={item.name}>{item.name}</span>
+          </li>
+        ))}
+        <li>End</li>
+      </ul>
+    );
+  }
+
+  const { getByTestId, asFragment } = render(
+    <DebeProvider
+      initialize={async db => {
+        const items = [];
+        for (let x = 0; x < 1000; x++) {
+          items.push({ name: `${x}`.padStart(3, '0') });
+        }
+        await db.insert('lorem', items);
+      }}
+      collections={collections}
+      getAdapter={() => new MemoryAdapter()}
+      render={() => <Component />}
+      loading={() => <span>Loading...</span>}
+    />
+  );
+
+  await (act as any)(async () => {
+    await waitForElement(() => getByTestId('result'));
+  });
+
+  expect(() => getByTestId('500')).toThrow();
+  expect(getByTestId('499')).toBeTruthy();
+  expect(asFragment()).toMatchSnapshot();
+  cb();
+});
+
+test('react:listen', async cb => {
+  let renders = 0;
+  function Component() {
+    const [result] = useAll<ILorem>('lorem', {});
+    renders = renders + 1;
+    return (
+      <ul>
+        {result.map(item => (
+          <li key={item.id}>
+            <span data-testid={item.name}>{item.name}</span>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  const { getByTestId, asFragment } = render(
+    <DebeProvider
+      initialize={async db => {
+        await db.insert('lorem', [{ name: 'Beni' }, { name: 'Alex' }]);
+        setTimeout(
+          () => db.insert('lorem', [{ name: 'Max' }, { name: 'Nik' }]),
+          1000
+        );
+      }}
+      collections={collections}
+      getAdapter={() => new MemoryAdapter()}
+      render={() => <Component />}
+      loading={() => <span>Loading...</span>}
+    />
+  );
+
+  await (act as any)(async () => {
+    await waitForElement(() => getByTestId('Nik'));
+  });
+
+  //expect(resultNode).toHaveTextContent();
+  expect(renders).toBe(3);
+  expect(asFragment()).toMatchSnapshot();
+  cb();
+});
+
+test('react:interact', async cb => {
+  function Component() {
+    const collection = useCollection<ILorem>('lorem');
+    const [result] = useAll<ILorem>('lorem', {});
+    return (
+      <div>
+        <button
+          data-testid="button"
+          onClick={() => collection.insert({ name: 'Jadda' })}
+        >
+          Add
+        </button>
+        <ul>
+          {result.map(item => (
+            <li key={item.id}>
+              <span data-testid={item.name}>{item.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  const { getByTestId, asFragment } = render(
+    <DebeProvider
+      initialize={async db => {
+        await db.insert('lorem', [{ name: 'Beni' }, { name: 'Alex' }]);
+      }}
+      collections={collections}
+      getAdapter={() => new MemoryAdapter()}
+      render={() => <Component />}
+      loading={() => <span>Loading...</span>}
+    />
+  );
+
+  await (act as any)(async () => {
+    await waitForElement(() => getByTestId('Beni'));
+  });
+
+  //expect(resultNode).toHaveTextContent();
+  await (act as any)(async () => {
+    fireEvent.click(getByTestId('button'));
+    await waitForElement(() => getByTestId('Jadda'));
+  });
+
+  expect(asFragment()).toMatchSnapshot();
+  cb();
+});
---
+++ src/react/react.tsx
@@ -0,0 +1,172 @@
+import * as React from 'react';
+import {
+  Debe,
+  IDebeUse,
+  IItem,
+  DebeAdapter,
+  ICollectionInput,
+  IQueryInput,
+  IGetItem
+} from 'debe';
+
+const context = React.createContext<Debe>(undefined as any);
+const Provider = context.Provider;
+
+export function useAllOnce<T>(
+  collection: string,
+  query: IQueryInput | string[]
+) {
+  return useBebeBase<T & IGetItem, (T & IGetItem)[]>(
+    collection,
+    (proxy, cb) => {
+      proxy.all(query).then(x => cb(x));
+    },
+    query,
+    []
+  );
+}
+
+export function useGetOnce<T>(collection: string, query: IQueryInput | string) {
+  return useBebeBase<(T & IGetItem) | undefined>(
+    collection,
+    (proxy, cb) => {
+      proxy.get(query).then(x => cb(x));
+    },
+    query,
+    undefined
+  );
+}
+
+export function useAll<T>(collection: string, query: IQueryInput | string[]) {
+  return useBebeBase<T & IGetItem, (T & IGetItem)[]>(
+    collection,
+    (proxy, cb) =>
+      proxy.all(query, res => {
+        cb(res);
+      }),
+    query,
+    []
+  );
+}
+
+export function useGet<T>(collection: string, query: IQueryInput | string) {
+  return useBebeBase<T & IGetItem, (T & IGetItem) | undefined>(
+    collection,
+    (proxy, cb) =>
+      proxy.get(query, res => {
+        cb(res);
+      }),
+    query,
+    undefined
+  );
+}
+
+function useBebeBase<TBase, TResult = TBase>(
+  collection: string,
+  handler: (
+    service: IDebeUse<TBase>,
+    cb: (res: TResult) => void
+  ) => void | (() => void),
+  arg: any,
+  defaultValue: TResult
+): [TResult, boolean] {
+  const [state, setState] = React.useState<{
+    res: TResult;
+    loading: boolean;
+    err?: any;
+  }>({
+    loading: true,
+    err: undefined,
+    res: defaultValue
+  });
+  const client = React.useContext(context);
+
+  React.useEffect(() => {
+    if (!client) {
+      return;
+    }
+    const proxy = client.use<TBase>(collection);
+    return handler(proxy, (res: TResult) => setState({ res, loading: false }));
+  }, [client, JSON.stringify(arg)]);
+  return [state.res, state.loading];
+}
+/*
+export function useDebeMethod<T>(
+  service: string,
+  method: string,
+  ...args: any[]
+): [T | undefined, boolean, any] {
+  const [state, setState] = React.useState<{
+    res?: number;
+    loading: boolean;
+    err?: any;
+  }>({
+    loading: true,
+    err: undefined,
+    res: undefined
+  });
+  const client = React.useContext(context);
+
+  React.useEffect(() => {
+    if (!client) {
+      return;
+    }
+    const proxy = client.use(service);
+    proxy[method](...args)
+      .then((res: any) =>
+        setState({
+          res: (res || undefined) as any,
+          loading: false,
+          err: undefined
+        })
+      )
+      .catch((err: any) => setState({ res: undefined, loading: false, err }));
+  }, [client, method, JSON.stringify(args)]);
+  return [state.res as any, state.loading, state.err];
+}*/
+
+export function useCollection<T = IItem>(service: string): IDebeUse<T> {
+  const debe = React.useContext(context);
+  return debe.use(service);
+}
+
+export function DebeProvider({
+  render,
+  children,
+  getAdapter,
+  loading,
+  collections = [],
+  initialize = () => Promise.resolve()
+}: {
+  initialize?: (db: Debe) => Promise<void>;
+  collections?: ICollectionInput[];
+  getAdapter: () => DebeAdapter;
+  render?: () => React.ReactNode;
+  authError?: () => React.ReactNode;
+  loading?: () => React.ReactNode;
+  children?: React.ReactNode;
+}) {
+  const [isInitialized, setState] = React.useState(false);
+  const debe = React.useMemo(() => new Debe(getAdapter(), collections), []);
+  React.useEffect(() => {
+    debe
+      .initialize()
+      .then(() => initialize(debe))
+      .then(() => {
+        setState(true);
+      })
+      .catch(err => console.log(err));
+    return () => {
+      // debe.close();
+    };
+  }, []);
+  let child = null;
+  if (!isInitialized && loading) {
+    child = loading();
+  } else if (isInitialized && render) {
+    child = render();
+  } else {
+    child = children;
+  }
+  return <Provider value={debe}>{child}</Provider>;
+}
